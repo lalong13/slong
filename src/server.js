41b53b911b78773110eead6bbbc685b5ab127431
Stephen Long <real.lalong13@gmail.com>
2017-05-08 22:23:48 -0700

    redirect to www

--- src/server.js
+++ src/server.js
@@ -29,35 +29,39 @@ app.use(function(req, res, next){
 
 // universal routing and rendering
 app.get('*', (req, res) => {
-  match(
-    { routes, location: req.url },
-    (err, redirectLocation, renderProps) => {
+    if (req.headers.host.match(/^www/) == null) {
+        return res.redirect(301, 'http://www.' + req.headers.host + req.url);
+    } else {
+        match(
+            { routes, location: req.url },
+            (err, redirectLocation, renderProps) => {
 
-      // in case of error display the error message
-      if (err) {
-        return res.status(500).send(err.message);
-      }
+          // in case of error display the error message
+                if (err) {
+                    return res.status(500).send(err.message);
+                }
 
-      // in case of redirect propagate the redirect to the browser
-      if (redirectLocation) {
-        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-      }
+          // in case of redirect propagate the redirect to the browser
+                if (redirectLocation) {
+                    return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+                }
 
-      // generate the React markup for the current route
-      let markup;
-      if (renderProps) {
-        // if the current route matched we have renderProps
-        markup = renderToString(<RouterContext {...renderProps}/>);
-      } else {
-        // otherwise we can render a 404 page
-        markup = renderToString(<NotFoundPage/>);
-        res.status(404);
-      }
+          // generate the React markup for the current route
+                let markup;
+                if (renderProps) {
+            // if the current route matched we have renderProps
+                    markup = renderToString(<RouterContext {...renderProps}/>);
+                } else {
+            // otherwise we can render a 404 page
+                    markup = renderToString(<NotFoundPage/>);
+                    res.status(404);
+                }
 
-      // render the index template with the embedded React markup
-      return res.render('index', { markup });
+          // render the index template with the embedded React markup
+                return res.render('index', { markup });
+            }
+        );
     }
-  );
 });
 
 // start the server
