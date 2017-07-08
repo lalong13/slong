5b41fadd138924d73287345def9e7b87a4327dfc
Stephen Long <real.lalong13@gmail.com>
2017-07-08 12:40:55 -0700

    Created Personal Interests Page

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,19 +3,18 @@ import React from 'react';
 import { Switch } from 'react-router';
 import { Route } from 'react-router-dom';
 import IndexPage from './IndexPage';
+import InterestsPage from './InterestsPage';
 import NotFoundPage from './NotFoundPage';
 
 //Page Container
 export default class MainContent extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
             <div className={"app-content w3-show w3-container"}>
                 <Switch>
                     <Route exact path="/" component={IndexPage}/>
+                    <Route exact path="/interests" component={InterestsPage}/>
                     <Route status={404} component={NotFoundPage}/>
                 </Switch>
             </div>
