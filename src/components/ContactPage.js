65e71bd1a5632fc6fa1fac4574f3f1a9ae9a1df8
Stephen Long <real.lalong13@gmail.com>
2017-07-11 23:50:10 -0700

    Created contact page, updated personal interests page, fixed mobile view bug on sidebar

---
+++ src/components/ContactPage.js
@@ -0,0 +1,23 @@
+// src/components/ContactPage.js
+import React from 'react';
+
+export default class ContactPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="home">
+                <div>
+                    <h1 className="w3-center">Please Contact Me</h1>
+                    <h4 className="w3-center">Feel free to email me or contact me on Github<br />Mention the site in the Subject line of the email to get a response</h4>
+                </div>
+                <div className="w3-block w3-center">
+                    <a className="gmail" href="mailto:real.lalong13@gmail.com?Subject=StephenLong%20Jedimasters"><img className="w3-center gmail-img" src="/img/New_Logo_Gmail.svg" /></a>
+                    <a href="" className="fa fa-icon fa-github"/>
+                </div>
+            </div>
+        );
+    }
+}
